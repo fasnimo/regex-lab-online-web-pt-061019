727da9d14e893d2be3beafa135d6ea283a1348e3
Rene Cocom <fasnimo@gmail.com>
2019-07-10 04:28:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,5 +26,5 @@ def valid_phone_number?(phone)
 # else
 #   return true
 # end
- phone.scan(/\b\(?\d{3}\)?\s?-?\d{3}-?\s?\d{4}\b/) ? true : false
+ phone.scan.empty?(/\b\(?\d{3}\)?\s?-?\d{3}-?\s?\d{4}\b/) ? true : false
 end
