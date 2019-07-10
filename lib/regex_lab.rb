a0dd5dbd5cb0ac7ea0923f2aed87b5288191fddd
Rene Cocom <fasnimo@gmail.com>
2019-07-10 04:17:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,10 +12,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].+[\!.?]$/)
-    return true
-  else
-    return false
+  text.match(/^[A-Z].+[\!.?]$/) ? true : false
+  
 end
 end
 
